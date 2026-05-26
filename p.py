dc6571542abfc95cf5c03e30b2b309047e90bbef
Sergio Parilli <sergioparilli06@gmail.com>
2026-05-26 15:06:31 -0400

    Fix MongoDB connection string and update print message

--- p.py
+++ p.py
@@ -1,9 +1,9 @@
 from pymongo import MongoClient
 
 try:
-    client = MongoClient("mongodb+srv://admin:admin12345@clusterprueba.l2t7dwu.mongodb.net/?appName=clusterPrueba")
+    client = MongoClient("mongodb+srv://@clusterprueba.l2t7dwu.mongodb.net/?appName=clusterPrueba")
     # Intentamos pedirle algo a la base de datos
     client.admin.command('ping')
-    print("✅ ¡Coronamos, Sergio! Conectado a MongoDB con éxito.")
+    print("✅ ¡Coronamos")
 except Exception as e:
     print(f"❌ Error de conexión: {e}")
